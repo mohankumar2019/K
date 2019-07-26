0c0ba5a144400c6f0d526cc549b659fe6c1d8066
mohan <mohan.chanti4@gmail.com>
2019-07-26 12:35:31 +0530

    new changes

---
+++ src/main/java/org/com/app/mvnK/App.java
@@ -0,0 +1,13 @@
+package org.com.app.mvnK;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "mvnK" );
+    }
+}
